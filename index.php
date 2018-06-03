d444c8d5a56f232f3d8670e00cf2387633b5c527
Сергий <s.p.sadchikov@gmail.com>
2018-06-03 13:02:25 +0500

    corect 1

--- index.php
+++ index.php
@@ -1,19 +1,27 @@
-<?php header("Content-Type: text/html; charset=utf-8");
+<?php
 
 $cityName = 'yekaterinburg';
-$city = file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=$cityName&APPID=3f22f3fb80167efc601a9167886ec893");
-$data = [];
-$data = json_decode($city,true);
+$link = "http://api.openweathermap.org/data/2.5/weather?q=$cityName&APPID=3f22f3fb80167efc601a9167886ec893";
 
-$cityName =($data['name']);
-$weather_icon = ($data['weather'][0]['icon']);
-$weather_type = ($data['weather'][0]['description']); 
-$temperarure = ($data['main']['temp']);
-$pressure = ($data['main']['pressure']); 
-$humidity = ($data['main']['humidity']);
-$windspeed = ($data['wind']['speed']);
-$sunrise = ($data['sys']['sunrise']);
-$sunset = ($data['sys']['sunset']);
+$city = file_get_contents($link);
+if ($city === false) {
+	exit('Не удалось получить данные');
+}
+
+$data = json_decode($city,true);
+if ($data === NULL) {
+	exit('Ошибка декодирования JSON');
+}
+
+!empty($data['name']) ? $cityName = $data['name'] : 'не удалось получить название города';
+!empty($data['weather'][0]['icon']) ? $weather_icon = $data['weather'][0]['icon'] : 'изображение недоступно';
+!empty($data['weather'][0]['description']) ? $weather_type = $data['weather'][0]['description'] : 'описание недоступно';
+!empty($data['main']['temp']) ? $temperarure = $data['main']['temp'] : 'данные отсутствуют';
+!empty($data['main']['pressure']) ? $pressure = $data['main']['pressure'] : 'данные отсутствуют';
+!empty($data['main']['humidity']) ? $humidity = $data['main']['humidity'] : 'данные отсутствуют';
+!empty($data['wind']['speed']) ? $windspeed = $data['wind']['speed'] : 'данные отсутствуют';
+!empty($data['sys']['sunrise']) ? $sunrise = $data['sys']['sunrise'] : 'данные отсутствуют';
+!empty($data['sys']['sunset']) ? $sunset = $data['sys']['sunset'] : 'данные отсутствуют';
 
 $pressure = $pressure * 0.75006375541921; 
 $pressure = round($pressure); 
@@ -24,9 +32,20 @@ $temperarure = round($temperarure-273.15);
 $sunrise = date('H:i',$sunrise);
 $sunset = date('H:i',$sunset);
 
-$title = "Давление: ".$pressure." мм рт. ст.\nВетер: ".$windspeed." м/с \nВлажность: ".$humidity."% \nВосход: ".$sunrise."\nЗакат: ".$sunset;
-echo "<h2>$cityName</h2><br><h3>$weather_type</h3>";
-echo ("<img src='http://openweathermap.org/img/w/$weather_icon.png'alt=\"$weather_type\" title=\"$title\" >"); 
-echo ("$temperarure<sup>o</sup>C<br>");
- 
- ?>
+$title = "Давление: ".$pressure." мм рт. ст.<br>Ветер: ".$windspeed." м/с<br>Влажность: ".$humidity."%<br>Восход: ".$sunrise."<br>Закат: ".$sunset;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Погода в Екатеринбурге</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h2><?php echo $cityName; ?></h2>
+	<h3><?php echo $weather_type; ?></h3>
+	
+	<div><img src="http://openweathermap.org/img/w/<?php echo $weather_icon ?>.png" title="<?php echo ($weather_type) ?>"><?php echo $temperarure; ?><sup>o</sup>C</div>
+	<div><?php echo $title; ?></div>
+</body>
+</html>
